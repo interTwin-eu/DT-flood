521a7bdd5d0e8c08af5b6159c27f78d02d6d495d
WOTrompDeltares <willem.tromp@deltares.nl>
2024-01-29 10:58:13 +0100

    Updated env; Notebook for WFLOW model

---
+++ Notebooks/utils/plot_utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import cartopy.crs as ccrs
+import cartopy.io.img_tiles as cimgt
+
+proj = ccrs.PlateCarree()
+
+from hydromt_wflow import WflowModel
+
+
+def plot_wflow_model(wflow_model):
+    gdf_bas = wflow_model.basins
+    gdf_riv = wflow_model.rivers
+    dem = wflow_model.staticmaps['wflow_dem'].raster.mask_nodata() 
+
+    vmin, vmax = dem.quantile([0., 0.98]).compute()
+    c_dem = plt.cm.terrain(np.linspace(0.25, 1, 256))
+    cmap = colors.LinearSegmentedColormap.from_list("dem", c_dem)
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+    cmap_kwargs = dict(cmap=cmap, norm=norm) 
+
+    fig = plt.figure(figsize=(14,7))
+    ax = fig.add_subplot(projection=proj)
+    ax.add_image(cimgt.QuadtreeTiles(),10,alpha=0.5)
+    gdf_bas.boundary.plot(ax=ax, color='k')
+    gdf_riv.plot(ax=ax,color='b', linewidth = gdf_riv['strord']/2)
+    dem.plot(ax=ax,**cmap_kwargs)
+
+    resv_kwargs = dict(facecolor='blue', edgecolor='black', linewidth=1)
+    wflow_model.staticgeoms['reservoirs'].plot(ax=ax,**resv_kwargs)
