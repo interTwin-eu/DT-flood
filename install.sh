7b67f36188a82ba8a8f19bc39c173609faaa4850
WOTrompDeltares <willem.tromp@deltares.nl>
2024-12-09 17:35:04 +0100

    update env install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mamba env create -f environment.yml -n de372_demo
-# mamba activate de372_demo
-source $CONDA_PREFIX/bin/activate de372_demo
-python3 -m pip install dfm_tools earthkit jupyter solara ipyleaflet mapbox-earcut localtileserver html2image
+source $HOME/miniforge3/bin/activate de372_demo
+python3 -m pip --no-cache-dir install dfm_tools earthkit jupyter solara ipyleaflet mapbox-earcut localtileserver html2image
 pip install -e .
