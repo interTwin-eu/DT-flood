354bb139df4211de1ea59d636c9fd1fb2c96dd46
WOTrompDeltares <willem.tromp@deltares.nl>
2024-12-16 09:16:26 +0100

    add files ra2ce service, update sfincs, wflow service

--- DT_flood/workflows/cwl_OSCAR/oscar_services/sfincs_script.sh
+++ DT_flood/workflows/cwl_OSCAR/oscar_services/sfincs_script.sh
@@ -2,6 +2,6 @@ ID=`basename "$INPUT_FILE_PATH" | cut -d'_' -f1`
 OUTPUT_FILE="$TMP_OUTPUT_DIR"/"$ID"_sfincs_output.tar
 echo $OUTPUT_FILE
 tar -xvf  "$INPUT_FILE_PATH" -C /data/
-sfincs
+sfincs | tee sfincs_log.txt
 tar -cf sfincs_output.tar sfincs_map.nc sfincs_his.nc sfincs_log.txt
 mv /data/sfincs_output.tar  $OUTPUT_FILE
