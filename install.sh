165e0cf651527eae16b9bcd1f010655bbb8d55f1
WOTrompDeltares <willem.tromp@deltares.nl>
2025-04-11 15:08:12 +0200

    clean up main dir

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mamba env create -f environment.yml
-source $HOME/miniforge3/bin/activate DT-flood
-python3 -m pip --no-cache-dir install dfm_tools earthkit jupyter solara ipyleaflet mapbox-earcut localtileserver html2image minio oscar-python liboidcagent
-pip install -e .
